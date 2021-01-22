1ccc5c1180e37cfec1aa80ec1b01ae341e480d76
achal saharan <asaharan_be17@thapar.edu>
2021-01-22 12:44:13 +0530

    feat - addCol & remCol
    added feature to add columns and delete columns from the table

--- scripts/main.js
+++ scripts/main.js
@@ -11,32 +11,54 @@ const table = document.querySelector('table');
 // adding event listeners
 addRowBtn.addEventListener('click', addRow);
 remRowBtn.addEventListener('click', remRow);
-
+addColBtn.addEventListener('click', addCol);
+remColBtn.addEventListener('click', remCol);
+``
 
 // keeping track of the no of rows and cols
-let rows = 3;
-let cols = 3;
+// let no_rows = table.rows.length;
+// let no_cols = table.rows[0].cells.length;
 
 function addRow(event) {
     //creating row
     row = document.createElement('TR');
-    for(let i=0; i<3; i++){
-        cell = document.createElement('TD');
-        cell.innerText = i+1;
+    let noCols = table.rows[0].cells.length;
+    for(let i=0; i<noCols; i++){
+        let cell = document.createElement('TD');
+        cell.innerText = i;
         row.appendChild(cell);
     }
 
-    console.log(row);
     table.appendChild(row);
-
-    rows++;
 }
 
 function remRow(event){
     //geting the refrence of rows
     const allRows = table.rows;
-    console.log(allRows)
-    table.rows[rows-1].remove();
-    rows--;
+    let noRows = table.rows.length
+    table.rows[noRows-1].remove();
+}
+
+function addCol(event){
+    const allRows = table.rows;
+    let noRows = table.rows.length;
+    let noCols = table.rows[0].cells.length;
+
+    for(let i=0; i<noRows; i++){
+        //create a td element
+        let cell = document.createElement('TD');
+        cell.innerText = noCols;
+        allRows[i].appendChild(cell);
+    }
+}
+
+function remCol(event){
+    const allRows = table.rows;
+    let noRows = table.rows.length;
+    let noCols = table.rows[0].cells.length;
+
+    for(let i=0; i<noRows; i++){
+        allRows[i].cells[noCols-1].remove();
+    }
 }
 
